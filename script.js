5bc3fe7229c2a9a89f5f2a6ffd762efaf631f01c
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-07-25 12:27:51 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="row justify-content-center">
+            <h1>
+                MATH KING
+            </h1>
+            
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
